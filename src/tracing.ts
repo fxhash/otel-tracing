3cc2b8d8a30ef529956d2be6ad88572f8672be7c
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-04-27 16:25:18 +0200

    ✨ don't trace if disabled

--- src/tracing.ts
+++ src/tracing.ts
@@ -13,12 +13,14 @@ interface ITracingConfig {
   jaegerEndpoint: string
   serviceName: string
   logTraces?: boolean
+  enabled?: boolean
   ignoreOutgoingUrls?: IgnoreMatcher[]
   ignoreIncomingPaths?: IgnoreMatcher[]
 }
 
 const defaultTracingConfig: Partial<ITracingConfig> = {
   logTraces: true,
+  enabled: true,
 }
 
 export function tracing(config: ITracingConfig) {
@@ -27,56 +29,58 @@ export function tracing(config: ITracingConfig) {
     ...config,
   }
 
-  // a log message to inform about tracing enabled
-  console.log(
-    `📝  tracing enabled, pushing to jaeger: ${config.jaegerEndpoint}`
-  )
-
-  // compose the jaeger endpoint using provided root
-  const JAEGER_ENDPOINT = `${config.jaegerEndpoint}/api/traces`
-  
-  // we use the node tracing provider
-  const provider = new NodeTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: config.serviceName,
-    }),
-  })
-
-  // configure the exporters
-  const exporter = new JaegerExporter({
-    endpoint: JAEGER_ENDPOINT,
-  })
-  provider.addSpanProcessor(
-    new BatchSpanProcessor(exporter)
-  )
-  if (config.logTraces) {
-    provider.addSpanProcessor(
-      new BatchSpanProcessor(new ConsoleOneLineSpanExporter())
+  if (config.enabled) {
+    // a log message to inform about tracing enabled
+    console.log(
+      `📝  tracing enabled, pushing to jaeger: ${config.jaegerEndpoint}`
     )
-  }
-  // "activates" the provider
-  provider.register()
-
-  // we compose the ignore arrays with the default values
-  const ignoreOutgoingUrls: IgnoreMatcher[] = [
-    ...(config.ignoreOutgoingUrls || []),
-    JAEGER_ENDPOINT,
-  ]
-  const ignoreIncomingPaths: IgnoreMatcher[] = [
-    ...(config.ignoreIncomingPaths || []),
-    "/hello",
-    "/",
-  ]
-
-  registerInstrumentations({
-    tracerProvider: provider,
-    instrumentations: [
-      getNodeAutoInstrumentations({
-        "@opentelemetry/instrumentation-http": {
-          ignoreOutgoingUrls: ignoreOutgoingUrls,
-          ignoreIncomingPaths: ignoreIncomingPaths,
-        }
-      })
+  
+    // compose the jaeger endpoint using provided root
+    const JAEGER_ENDPOINT = `${config.jaegerEndpoint}/api/traces`
+    
+    // we use the node tracing provider
+    const provider = new NodeTracerProvider({
+      resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: config.serviceName,
+      }),
+    })
+  
+    // configure the exporters
+    const exporter = new JaegerExporter({
+      endpoint: JAEGER_ENDPOINT,
+    })
+    provider.addSpanProcessor(
+      new BatchSpanProcessor(exporter)
+    )
+    if (config.logTraces) {
+      provider.addSpanProcessor(
+        new BatchSpanProcessor(new ConsoleOneLineSpanExporter())
+      )
+    }
+    // "activates" the provider
+    provider.register()
+  
+    // we compose the ignore arrays with the default values
+    const ignoreOutgoingUrls: IgnoreMatcher[] = [
+      ...(config.ignoreOutgoingUrls || []),
+      JAEGER_ENDPOINT,
     ]
-  })
+    const ignoreIncomingPaths: IgnoreMatcher[] = [
+      ...(config.ignoreIncomingPaths || []),
+      "/hello",
+      "/",
+    ]
+  
+    registerInstrumentations({
+      tracerProvider: provider,
+      instrumentations: [
+        getNodeAutoInstrumentations({
+          "@opentelemetry/instrumentation-http": {
+            ignoreOutgoingUrls: ignoreOutgoingUrls,
+            ignoreIncomingPaths: ignoreIncomingPaths,
+          }
+        })
+      ]
+    })
+  }
 }
